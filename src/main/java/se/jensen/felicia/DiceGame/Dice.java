1654e902609475da983ed3f082c43d9d5cab5526
MissJacobi <feliciajacobi97.fj@gmail.com>
2025-09-21 11:36:17 +0200

    Dice Game!

--- src/main/java/se/jensen/felicia/DiceGame/Dice.java
+++ src/main/java/se/jensen/felicia/DiceGame/Dice.java
@@ -6,7 +6,6 @@ public class Dice {
     Random random = new Random();
 
     public void playerScore(Player player) {
-        int sum = 0;
         for(int i = 0; i < 2 ; i++)
         {
             int roll = random.nextInt(1, 7);
